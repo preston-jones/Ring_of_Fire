cead42cae1f78a1a132c32dcb90081a86cdbbdb3
Preston Jones <preston.jones@gmx.de>
2024-06-05 14:33:54 +0200

    edit game board

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,20 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
-
-  newPlayer: {
-    name: string,
-    drinks: number
-  }  = {
-    name: 'Preston',
-    drinks: 0
-  };
 
 }
