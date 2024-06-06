fee82b4f385bc35b9826b3a77a4bfa69138a1505
Preston Jones <preston.jones@gmx.de>
2024-06-06 13:09:39 +0200

    add card arrays

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -9,10 +10,34 @@ import { Component } from '@angular/core';
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
+  game!: Game;
+  currentCard: any;;
   flipCardAnimation: Boolean = false;
   takeCardAnimation: Boolean = false;
 
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
   takeCard() {
-    this.takeCardAnimation = true;
+    if (!this.takeCardAnimation && this.game.cardStack.length > 0) {
+      this.currentCard = this.game.cardStack.pop();
+      console.log(this.currentCard);
+      this.takeCardAnimation = true;
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.takeCardAnimation = false;
+        console.log(this.game.cardStack.length);
+      }, 1500);
+    }
+    else if (this.game.cardStack.length === 0){
+      alert('No more cards');
+    }
   }
 }
