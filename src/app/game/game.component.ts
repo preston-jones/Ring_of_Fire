4d783436d6c052225ce55c50d20c15e7bc7c0d8f
Preston Jones <preston.jones@gmx.de>
2024-06-28 00:37:33 +0200

    add updateGame Funktion

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -5,9 +5,10 @@ import { PlayerComponent } from '../player/player.component';
 import { InfoBoxComponent } from '../info-box/info-box.component';
 import { GamedataService } from '../gamedata.service';
 import { GameInterface } from '../interfaces/game.interface';
+import { ActivatedRoute } from '@angular/router';
 
 /* -- Imports for Firestore -- */
-import { Firestore, collectionData, collection, addDoc } from '@angular/fire/firestore';
+import { Firestore, collectionData, collection, addDoc, doc, onSnapshot, getDoc, updateDoc  } from '@angular/fire/firestore';
 
 /* -- Imports for Material Dialog and Button -- */
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
@@ -18,6 +19,7 @@ import { FormsModule } from '@angular/forms';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDialog} from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-game',
@@ -41,33 +43,49 @@ export class GameComponent {
   game!: Game;
   currentCard: any;
   gameData = inject(GamedataService);
+  gameId: string = '';
   // currentCard: string = ''; /* => ERROR: TS2322: Type 'string | undefined' is not assignable to type 'string'. Type 'undefined' is not assignable to type 'string'. */
   flipCardAnimation: Boolean = false;
   takeCardAnimation: Boolean = false;
 
-  constructor(public firestore: Firestore, public matDialog: MatDialog) {} /* Why must declare it in the constructor? */
+  constructor(private route: ActivatedRoute, public firestore: Firestore, public matDialog: MatDialog) {} /* Why must declare it in the constructor? */
 
   ngOnInit(): void {
     this.newGame();
+    this.getCurrentGame();
+
   }
+
 
   newGame() {
     this.game = new Game();
-    this.addNote(this.game, "games");
-    console.log(this.game);
-  }
-
-  async addNote(item: Game, colId: "games") {
-    await addDoc(this.getNotesRef(), this.getCleanJson(item)).catch(
-      (err) => { console.error(err) }
-    ).then(
-      (docRef) => console.log("Document written with ID: ", docRef?.id)
-    )
   }
 
 
-  getNotesRef() {
-    return collection(this.firestore, 'games');
+  getCurrentGame() { 
+    this.route.params.subscribe(async (params) => {
+      this.gameId = params['id'];
+      const currentGameRef = this.getGameRef();
+      const gameData = await getDoc(currentGameRef);
+      
+      if (gameData.exists()) {
+        let game = gameData.data();
+        this.game.cardStack = game['cardStack'];
+        this.game.players = game['players'];
+        this.game.playedCards = game['playedCards'];
+        this.gameData.currentPlayerGlobal = game['currentPlayer'];
+      }
+    });
+  }
+
+
+  getGameRef() {
+    return doc(this.firestore, "games/" + this.gameId);
+  }
+
+
+  getSingleGameRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
   }
 
 
@@ -81,9 +99,18 @@ export class GameComponent {
   }
 
 
+  async updateGame() {
+    let docRef = this.getSingleGameRef("games", this.gameId);
+    await updateDoc(docRef, this.getCleanJson(this.game)).catch(
+      (err) => { console.error(err) }
+    );
+  }
+
+
   takeCard() {
     if (!this.takeCardAnimation && this.game.cardStack.length > 0) {
       this.currentCard = this.game.cardStack.pop();
+      this.updateGame();
       this.gameData.currentCardGlobal = this.currentCard;
       this.takeCardAnimation = true;
       this.gameData.currentPlayerGlobal++;
@@ -92,6 +119,7 @@ export class GameComponent {
       setTimeout(() => {
         this.game.playedCards.push(this.currentCard);
         this.takeCardAnimation = false;
+        this.updateGame();
       }, 1500);
     }
     else if (this.game.cardStack.length === 0){
@@ -107,6 +135,7 @@ export class GameComponent {
     dialogRef.afterClosed().subscribe(name => {
       if (name && name.length > 0) {
         this.game.players.push(name);
+        this.updateGame();
       }
     });
   }
--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Game } from '../../models/game';
+import { Firestore, collectionData, collection, addDoc, doc, onSnapshot, getDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-start-screen',
@@ -9,13 +11,24 @@ import { Router } from '@angular/router';
   styleUrl: './start-screen.component.scss'
 })
 export class StartScreenComponent {
-  
-
-  constructor(private router: Router) { }
 
 
-  newGame() {
-    console.log('Starting a new game');
-    this.router.navigateByUrl("/game");
+  constructor(private firestore: Firestore, private router: Router) { }
+
+
+  async newGame() {
+    let game: Game = new Game();
+    game = {
+      players: game.players,
+      cardStack: game.cardStack,
+      playedCards: game.playedCards,
+      currentPlayer: 0,
+    }
+    let gameRef = collection(this.firestore, "games");
+    await addDoc(gameRef, game).catch(
+      (err) => { console.error(err) }
+    ).then(
+      (docRef) => {this.router.navigateByUrl("/game/" + docRef?.id)}
+    )
   }
 }
